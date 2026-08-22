72b3efd8731c96a91cbc47ac15fc2fa2decd9c3f
salomoneflor <salomoneflor@gmail.com>
2026-08-22 08:49:54 -0300

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,31 @@
+
+# Definición de una función que imprime un saludo
+def saludar(nombre):
+    print(f"¡Hola, {nombre}!")
+
+# Llamada a la función
+saludar("Flor")  
+
+# Definición de una función que imprime un saludo
+def saludar(nombre):
+    print(f"¡Hola, {nombre}!")
+# Llamada a la función
+saludar("Dami")
+
+# Definición de una función que imprime un saludo
+def saludar(nombre):
+    print(f"¡Hola, {nombre}!")
+# Llamada a la función
+saludar("Lalo")
+
+# Calcular área del rectángulo
+def calcular_area_rectangulo(base,altura):
+    print(f"El área del rectángilo es {base * altura}!")
+# Llamada a la función
+calcular_area_rectangulo(10,20)
+
+# Calcular área del rectángulo
+def calcular_area_rectangulo(base,altura):
+    print(f"El área del rectángilo es {base * altura}!")
+# Llamada a la función
+calcular_area_rectangulo(5,5) # Cambio valores para probar función
